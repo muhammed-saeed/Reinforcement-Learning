35142ac4b263d12f3e28a160daffd6c16f65f9cb
Muhammed Saeed <38116007+muhammed-saeed@users.noreply.github.com>
2021-03-05 19:51:54 +0200

    Update brain_ai.py

--- optimize_the_cost_using_dqn/brain_ai.py
+++ optimize_the_cost_using_dqn/brain_ai.py
@@ -7,8 +7,8 @@ class Brain(object):
     def __init__(self, learning_rate = 0.001, number_actions = 5):
         self.learning_rate = learning_rate
         states = Input(shape = (3,))
-        x = Dense(units = 64, activation = 'sigmoid')(states)
-        y = Dense(units = 32, activation = 'sigmoid')(x)
+        x = Dense(units = 128, activation = 'sigmoid')(states)
+        y = Dense(units = 64, activation = 'sigmoid')(x)
         q_values = Dense(units = number_actions, activation = 'softmax')(y)
         self.model = Model(inputs = states, outputs = q_values)
         self.model.compile(loss = 'mse', optimizer = Adam(learning_rate))
